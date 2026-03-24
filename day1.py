7e109757309a7d3abd91951ae2b9e1c661b1c1ba
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-24 15:35:03 +0530

    First commit

---
+++ day1.py
@@ -0,0 +1,114 @@
+
+# var1="Sajid"
+# print(var1)
+# print(type(var1))
+
+# Dry Principal --> Do not repeat
+
+# var165=78
+
+# rftgva45645r165454=78
+# rftgv@#$%^=78
+# age=89
+
+# age=67
+# AGE=56
+
+# print(age)
+# print(AGE)
+
+# str1=67
+# print(str1)
+# print(type(str1))
+
+# str1="Rohan"
+# print(str1)
+# print(type(str1))
+
+# str1=67.45
+# print(str1)
+# print(type(str1))
+
+# str1=True
+# print(str1)
+# print(type(str1))
+
+# list1=[4,6,3,2,4]
+# print(list1)
+# print(type(list1))
+
+# list1=(4,6,3,2,4)
+# print(list1)
+# print(type(list1))
+
+# list1={4,6,3,2,}
+# print(list1)
+# print(type(list1))
+
+# a=5
+# b=6.4
+# print(a+b)
+# print(type(a+b))
+
+# print(int(a+b))
+# print(type(int(a+b)))
+
+# var1=input("Enter your age: ")
+# print("My age is :",var1)
+
+# a=int(input("Enter the number1: "))
+# b=int(input("Enter the number2: "))
+
+# print(a+b)
+
+# str1="Rohan"
+# str2="Kumar"
+
+# print(str1+str2)
+
+# a=int(input("Enter the the side of square: "))
+# print("Area",a*a)
+# print("Perimeter",4*a)
+
+# a=3
+# b=5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b) # how many times without point value 3
+
+# print(a%b) # remainder value 3
+# print(a**b) # power a raise to b
+
+# a=4
+# b=4
+
+# print(a>b) # true false
+# print(a<b)
+# print(a<=b)
+# print(a>=b)
+# print(a==b)
+# print(a!=b)
+
+# a=5
+
+# a=a+6 a+=5
+
+# a+=6 
+# a*=2
+# a/=5
+# a-=2
+
+# print(a)
+
+# a=5
+# b=3
+
+
+# print(15>7 and 4<6 and 3>=9)
+# print(15>7 and 4<6 and 13>=9)
+
+# print(5>7 or 4<6 or 3>=9)
+# print(5>7 or 14<6 or 3>=9)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+list1=[4,6,3,2,4]
+print(list1)
+print(type(list1))
