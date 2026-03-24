2efee537bd50296155e512b45cfe052090d6e2a5
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-24 16:26:13 +0530

    strings methods

---
+++ day2.py
@@ -0,0 +1,81 @@
+
+# str1="amit-34-2000"
+
+# str1='This is my string hello \nhow are you'
+
+# str1="""
+# Hello i am good
+# how are you
+#     i am fine
+# """
+
+# print(str1)
+# str1="Hello"
+# str2="Sumit"
+# str3="Keshav"
+
+# print(str1+str2)
+
+# print(str1,str2,str3,sep="/")
+
+# print(str1,end=" ")
+# print(str2)
+# print(str3)
+
+
+# str1="Sajid Sir How are you"
+
+# print(str1.upper())
+# print(str1.lower())
+
+str2="Keshav Rao Hello"
+
+# print(len(str2))
+
+# print(str2[0])
+# print(str2[1])
+
+# print(str2[0:3])
+# print(str2[7:10])
+# print(str2[7:])
+# print(str2[:10])
+
+# str2="Keshav Rao Hello"
+
+# str2="      Abhinay      "
+
+# str2="***Abhinay***"
+
+# print(str2.upper())
+# print(str2.upper())
+# print(str2)
+
+# print(str2.lstrip("*"))
+# print(str2.rstrip("*"))
+
+# str2="Keshav Rao Hello"  
+
+# str3="sumit kumar kaise ho"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+# print(str2.replace("Keshav","Sumit"))
+# print(str3.title())
+# print(str3.capitalize())
+
+# str1 = "Welcome to the Console!!!"
+# print(str1.center(50, "."))
+
+# str2 = "Abracadabra"
+# countStr = str2.count("a")
+# print(countStr)
+
+# str1 = "Welcome to the Console!!!"
+
+# print(str1.endswith("!!!"))
+# print(str1.startswith("Wel"))
+# print(str1.find("e"))
+
+# age=63
+
+# if(age>18):
+#     print("you can vote")
+# else:
+#     print("you can not vote")
