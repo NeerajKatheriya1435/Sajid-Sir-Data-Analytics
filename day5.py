f817af642c136afa8e9a4e8b8ce2f99b096e5b89
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-04-03 16:10:54 +0530

    function return and list tuple

---
+++ day5.py
@@ -0,0 +1,118 @@
+
+# a=8
+# b=7
+# print(a+b)
+
+# c=6
+# d=2
+# print(c+d)
+
+# def addTwoNum():
+#     a=7
+#     b=5
+#     print(a+b)
+
+# def addTwoNum(num1,num2):
+#     a=num1
+#     b=num2
+#     print(a+b)
+
+# addTwoNum()
+
+# print("Hello Program")
+
+# l1=[5,78,78]
+
+# print(len(l1))
+# addTwoNum(4,9)
+# addTwoNum(2,7)
+# addTwoNum("A","B")
+
+# def average(num1,num2,num3):
+#     avg=(num1+num2+num3)/3
+#     print(avg)
+
+
+# average(3,4,5)
+# average(2,6,9)
+
+# def addList(list1):
+#     sum=0
+#     for i in list1:
+#         sum+=i
+#     print(sum/len(list1))
+
+# l1=[3,5,6,7,8,9,9]
+
+# addList(l1)
+
+# def average(num1,num2,num3):
+#     avg=(num1+num2+num3)/3
+#     # print(avg)
+#     return avg
+
+# print(average(4,7,9))
+
+# lsit1=[5,7,8,9,5,3,"Hello","Rohan"]
+# print(lsit1)
+
+# l1=[4,6,8,9,1,5,7,8]
+
+# print(l1)
+# print(l1[3])
+# print(l1[2:7])
+
+# print(l1[:7])
+# print(l1[2:])
+
+# for item in l1:
+#     print(item)
+
+# l1=[4,6,8,9]
+
+# print(l1[-4])
+# l1.sort()
+# l1.reverse()
+
+# print(l1.index(6))
+
+# print(l1.count(9))
+
+# l2=l1.copy()
+# l1.append(34)
+# l1.append(99)
+# l1.append(12)
+
+# l1=[4,6,8,9]
+# l1.insert(1,45)
+# print(l1)
+
+# l1=[4,6,8,9]
+# l2=[56,89]
+
+# l1.extend(l2)
+
+# print(l1)
+
+# print(l1+l2)
+
+# tuple1=(5,7,3,4,3)
+
+# dob=("12","06","2026")
+
+# print(tuple1)
+# print(tuple1[0])
+# print(tuple1.count(3))
+# print(tuple1.index(5))
+
+# for item in tuple1:
+#     print(item)
+
+# tuple1=(5,7,3,4,3)
+
+# l1=list(tuple1)
+# l1.append(9999)
+# l1.sort()
+# tuple1=tuple(l1)
+
+# print(tuple1)
