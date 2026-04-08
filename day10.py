207009f0f864372db54e38c57e4e8af9f51f1e5e
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-04-08 16:00:25 +0530

    Oops and constructor

---
+++ day10.py
@@ -0,0 +1,29 @@
+# class Human:
+
+#     def __init__(self,name,age):
+#      self.name=name
+#      self.age=age
+#      print("Hello Constructor")
+
+#     def details(self):
+#        print(f"My name is: {self.name} and age is: {self.age}")
+
+
+# h1=Human("Rohan",45)
+# h1.details()
+
+# Human(h1,name,age)
+# Human.details(h1)
+
+# h2=Human("Shivam",34)
+# h2.details()
+
+# print(h2.name)
+# print(h2.age)
+# Human.details(h2)
+
+# h3=Human("Sahil",36)
+# h3.details()
+
+h4=Human("Jazaul",78)
+h4.details()
---
+++ day9.py
@@ -0,0 +1,41 @@
+"""
+Human
+    ->Run
+    ->Laugh
+    ->Walk
+    ->Eat
+    ->Drink
+"""
+
+"""
+Teacher->Teach
+Programmer-> Program
+Student -> Study
+Plumber
+Electrician
+"""
+
+class Human:
+    name="Rohan"
+    age=34
+    course="Diploma"
+
+h1=Human()
+h1.name="Sumit"
+h1.age=23
+print(h1.name)
+print(h1.age)
+print("----------")
+
+h2=Human()
+h2.name="John"
+h2.age=67
+print(h2.name)
+print(h2.age)
+print("----------")
+
+h3=Human()
+h3.name="Peter"
+h3.age=24
+print(h3.name)
+print(h3.age)
