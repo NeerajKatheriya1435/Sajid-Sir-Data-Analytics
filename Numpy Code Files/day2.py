f66e79105547089916de1b741ad957f0e830d7bc
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-04 18:04:27 +0530

    replace and fillna dropna

--- Numpy Code Files/day2.py
+++ Numpy Code Files/day2.py
@@ -1,15 +1,37 @@
-import time
+# import time
 import numpy as np
 
-s1=time.time()
-l1=[i*5 for i in range(1,20)]
-e1=time.time()
+# s1=time.time()
+# l1=[i*5 for i in range(1,20)]
+# e1=time.time()
 
-s2=time.time()
-l2=np.arange(1,20)*5
-e2=time.time()
+# s2=time.time()
+# l2=np.arange(1,20)*5
+# e2=time.time()
 
-# print(e1-s1)
-# print(e2-s2)
-print(l1)
-print(l2)
+# # print(e1-s1)
+# # print(e2-s2)
+# print(l1)
+# print(l2)
+# 
+# arr = np.array([10, 20, 30, 40])
+
+# copiendArr=arr.copy()
+# arr[2]=90
+
+# copiendArr=arr
+# arr[2]=90
+
+# print("Original",arr)
+# print("Copy Array",copiendArr)
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+
+# cpy=arr.copy()
+cpy=arr
+cpy[1]=90
+
+print(arr)
+print(cpy)
